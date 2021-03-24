21b84f45b37952dbcc0a5924fb957eaf23d5e99a
JA-003 <79385487+JA-003@users.noreply.github.com>
2021-03-23 20:04:24 -0400

    completed exercise

--- src/js/index.js
+++ src/js/index.js
@@ -33,14 +33,34 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "<i>Name</i>"} ${
+    variables.lastname ? variables.lastname : "<i>Lastname</i>"
+  }</h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""}, ${
+    variables.country ? variables.country : ""
+  }</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li${
+              variables.twitter ? "" : ' style = "background-color: #b6c5c4;"'
+            }><a href="https://twitter.com/${
+    variables.twitter ? variables.twitter : ""
+  }"><i class="fa fa-twitter"></i></a></li>
+            <li${
+              variables.github ? "" : ' style = "background-color: #b6c5c4;"'
+            }><a href="https://github.com/${
+    variables.github ? variables.github : ""
+  }"><i class="fa fa-github"></i></a></li>
+            <li${
+              variables.linkedin ? "" : ' style = "background-color: #b6c5c4;"'
+            }><a href="https://linkedin.com/${
+    variables.linkedin ? variables.linkedin : ""
+  }"><i class="fa fa-linkedin"></i></a></li>
+            <li${
+              variables.instagram ? "" : ' style = "background-color: #b6c5c4;"'
+            }><a href="https://instagram.com/${
+    variables.instagram ? variables.instagram : ""
+  }"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
